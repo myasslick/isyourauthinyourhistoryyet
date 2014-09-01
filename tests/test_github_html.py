d55801fd14129ef0a06aea8ee993f5902b512b42
Yeuk Hon Wong <yeukhon@acm.org>
2014-09-01 23:59:15 +0000

    Added search generator and more tests.

--- tests/test_github_html.py
+++ tests/test_github_html.py
@@ -1,18 +1,23 @@
+import os
 import unittest
 try:
-    from unitest.mock import patch, MagicMock
+    from unitest.mock import patch, MagicMock, call
 except ImportError:
-    from mock import patch, MagicMock
+    from mock import patch, MagicMock, call
 from bs4 import BeautifulSoup as BS
 
 from isyourauthinyourhistoryyet import github
+
+FIXTURE_DIR = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+     "fixtures")
 
 class TestMockGithubHTML(unittest.TestCase):
 
     @patch("requests.get")
     def test_1search_code_returns_text(self, requests):
         requests.return_value = MagicMock(text="123")
-        html = github.search_code()
+        html = github.search_code("Python", "hello world", 1)
         self.assertEqual(html, "123")
 
     def test_get_repo_link_with_beginning_slash(self):
@@ -23,10 +28,43 @@ class TestMockGithubHTML(unittest.TestCase):
         self.assertEqual(link,
             github.GITHUB_URL + "name/repo")
 
+    @patch("isyourauthinyourhistoryyet.github.BS")
+    def test_get_code_list(self, bs):
+        html = "html"
+        bs.return_value.findAll.return_value = range(0,2)
+        code_items = github.get_code_list(html)
+        self.assertEqual(code_items, range(0,2))
+
+    @patch("isyourauthinyourhistoryyet.github.get_repo_link")
+    @patch("isyourauthinyourhistoryyet.github.get_code_list")
+    @patch("isyourauthinyourhistoryyet.github.search_code")
+    def test_search_max_2(self, search_code, get_code_list, get_repo_link):
+        html = "html"
+        search_code.return_value = html
+        code_lists = [
+            ["/user1/repo1"],
+            ["/user2/repo2"]
+        ]
+        link_lists = [link for links in code_lists for link in links]
+        get_code_list.side_effect = code_lists
+        get_repo_link.side_effect = link_lists
+
+        generator = github.search(max_page=2)
+        self.assertEqual(generator.next(), link_lists[0])
+        self.assertEqual(generator.next(), link_lists[1])
+        self.assertRaises(StopIteration, generator.next)
+
+        get_code_list.assert_has_calls([call(html), call(html)])
+        get_repo_link.assert_has_calls(
+            [
+                call(code_lists[0][0]),
+                call(code_lists[1][0])
+            ]
+        )
 class TestGithubHTML(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        html = github.search_code(language="Python", q="hello world")
+        html = github.search_code("Python", "hello world", 1)
         cls.soup = BS(html)
         
     def test_github_default_to_best_match(self):
@@ -40,8 +78,7 @@ class TestGithubHTML(unittest.TestCase):
         self.assertEqual(current[0].text, "1")
 
     def test_search_page2_is_on_page2(self):
-        html = github.search_code(language="Python", q="hello world",
-                    page=2)
+        html = github.search_code("Python", "hello world", 2)
         soup = BS(html)
         current = soup.findAll("em", {"class": "current"})
         self.assertEqual(len(current), 1)
@@ -56,3 +93,21 @@ class TestGithubHTML(unittest.TestCase):
         repo_href = code_list_items[0].find("p").a
         self.assertTrue(repo_href)
         self.assertEqual(repo_href["href"], "/" + repo_href.text)
+
+    @patch("isyourauthinyourhistoryyet.github.search_code")
+    def test_search_page_1_2(self, search_code):
+        file1 = os.path.join(FIXTURE_DIR, "hello_world_page_1.html")
+        fil2 = os.path.join(FIXTURE_DIR, "hello_world_page_2.html")
+
+        with open(file1, "r") as f:
+            f1 = f.read()
+        with open(fil2, "r") as f:
+            f2 = f.read()
+        search_code.side_effect = [f1, f2]
+        # there are 10 repo links per each file
+        links = []
+        for link in github.search(max_page=2):
+            links.append(link)
+        self.assertEqual(len(links), 20)
+        self.assertEqual(links[0], "https://github.com/palodequeso/Element-Games-Engine")
+        self.assertEqual(links[19], "https://github.com/ordavidil/hackita")
