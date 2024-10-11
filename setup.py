c889c92abf15f1fe327d1e49c03676b4ded511c4
myasslick <myasslick808@gmail.com>
2024-10-11 18:50:59 +0800

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name="isyourauthinyourhistoryyet",
-      version="0.0",
-      author="Yeuk Hon Wong",
-      author_email="yeukhon@acm.org",
-      packages=find_packages(),
-      include_package_data=True,
-)
